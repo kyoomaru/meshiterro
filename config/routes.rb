354d642f8aaeaec0f2e8bfc3781fe0a16ac7f276
kyoomaru <138374899+kyoomaru@users.noreply.github.com>
2023-08-04 05:41:10 +0000

    [Finish]12章

---
+++ config/routes.rb
@@ -0,0 +1,10 @@
+Rails.application.routes.draw do
+  resources :post_images, only: [:new, :create, :index, :show]
+  
+  
+  devise_for :users
+  root to: 'homes#top'
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get '/homes/about' => 'homes#top', as: 'about'
+end
+ 
